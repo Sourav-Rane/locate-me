b455ee6a264e1d3d05182ac1bc2e7457860e721f
Sourav Rane <44705241+Sourav-Rane@users.noreply.github.com>
2020-12-14 17:47:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [address, setAddress] = useState("");
+
+  const [zip, setZip] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+
+  const locateMe = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        axios
+          .get(
+            ` https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${position.coords.latitude}&lon=${position.coords.longitude}&zoom=18&addressdetails=1 `
+          )
+          .then((res) => {
+            console.log(res);
+            const obj = res.data.address;
+            setZip(obj.postcode);
+            setCountry(obj.country);
+            setState(obj.state);
+          })
+          .catch();
+      });
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={locateMe}>Auto Fill address</button>
+      <form>
+        ZIP:<input value={zip}></input>
+        <br />
+        Country:<input value={country}></input>
+        <br />
+        State:<input value={state}></input>
+        <br />
+      </form>
     </div>
   );
 }
